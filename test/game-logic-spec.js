034fe3069d379cea62b46530d019c82a79233e74
Forsaken <alexey2142@mail.ru>
2015-04-25 16:21:33 +1000

    Тесты на защиту и атаку

--- test/game-logic-spec.js
+++ test/game-logic-spec.js
@@ -88,4 +88,57 @@ describe("game logic", function() {
       })
     })
   })
+
+  it('should send tick after start game and second user defence', function(done) {
+    var client1 = io.connect(socketURL, options)
+    client1.on('connect', function() {
+      client1.emit('play', player1())
+      client1.on('waiting', function() {
+        var client2 = io.connect(socketURL, options)
+        client2.on('connect', function() {
+          var p2 = player2()
+          client2.emit('play', p2)
+          client2.on('start', function(msg) {
+            client2.emit('defence_start', { id: p2.id })
+            client2.on('tick', function(msg) {
+              msg[p2.id].id.should.equal(p2.id)
+              msg[p2.id].name.should.equal('Andrey')
+              msg[p2.id].action.should.equal('d')
+              client2.emit('defence_stop', { id: p2.id })
+              client1.disconnect()
+              client2.disconnect()
+              done()
+            })
+          })
+        })
+      })
+    })
+  })
+
+  it('should send tick after start game and first user attack', function(done) {
+    var client1 = io.connect(socketURL, options)
+    client1.on('connect', function() {
+      var p1 = player1()
+      client1.emit('play', p1)
+      client1.on('waiting', function() {
+        var client2 = io.connect(socketURL, options)
+        client2.on('connect', function() {
+          var p2 = player2()
+          client2.emit('play', p2)
+          client2.on('start', function(msg) {
+            client1.emit('attack', { id: p1.id })
+            client1.on('tick', function(msg) {
+              msg[p1.id].id.should.equal(p1.id)
+              msg[p1.id].name.should.equal('Alexey')
+              msg[p1.id].mana.should.notEqual(100)
+              msg[p2.id].health.should.notEqual(100)
+              client1.disconnect()
+              client2.disconnect()
+              done()
+            })
+          })
+        })
+      })
+    })
+  })
 })
